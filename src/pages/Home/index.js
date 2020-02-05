5083ee897f0253c2409eb376de0055b68dde792d
william <4269946@qq.com>
2020-02-05 16:27:31 +0800

    add touch event for Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,46 +1,81 @@
 import Time from "./Time";
 import Calendar from "./Calendar";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './index.css'
 
 function Home() {
 
+  const [startX, setStartX] = useState(0);
+  const [startY, setStartY] = useState(0);
+  const [scrolling, isScrolling] = useState(false);
+  const [sIndex , setSIndex] = useState(0);
+
   useEffect(() => {
+    let main = document.querySelector('.main'); //需要移动的DOM
     let sHeight = document.querySelector('.a').clientHeight;
 //获取一屏的高度，判断该移动多少
     let sLength = document.querySelectorAll('.section').length;
 //判断有几屏页面
-    let sIndex = 0;//标记当前第几页
-    let sScroll = false;//滚动时就不再触发事件，防止误点
-    let main = document.querySelector('.main'); //需要移动的DOM
+    function scrollTo(i) {
+      console.log(sIndex);
+      //操作动画的函数
+      isScrolling(true);
+      main.style.top = -i * sHeight + 'px';
+      setSIndex(i);
+      setTimeout(function () {
+        isScrolling(false);
+      }, 1000);
+    }
+
     window.onmousewheel = function (e) {
       //PC端的滚轮事件，嗯不兼容火狐
-      if(!sScroll) {
+      if(!scrolling) {
         if(e.wheelDelta > 0) {
           //上一页
           if(sIndex === 0) {
             return;
           }
-          sIndex--;
-          scrollTo(sIndex);
+          scrollTo(sIndex -1);
         } else {
           //下一页
           if(sIndex === sLength - 1) {
             return;
           }
-          sIndex++;
-          scrollTo(sIndex);
+          scrollTo(sIndex + 1);
         }
       }
     };
+    window.ontouchstart = function (e) {
+      setStartX(e.changedTouches[0].pageX);
+      setStartY(e.changedTouches[0].pageY)
+    };
+    window.ontouchmove = function (e) {
+      if(!scrolling) {
+        const endX = e.changedTouches[0].pageX;
+        const endY = e.changedTouches[0].pageY;
+        const distanceX = endX - startX;
+        const distanceY = endY - startY;
+        if(Math.abs(distanceX) > Math.abs(distanceY) && distanceX > 0) {
+          console.log('往右滑动');
+        } else if(Math.abs(distanceX) > Math.abs(distanceY) && distanceX < 0) {
+          console.log('往左滑动');
+        } else if(Math.abs(distanceX) < Math.abs(distanceY) && distanceY < 0) {
 
-    function scrollTo(i) {
-      //操作动画的函数
-      sScroll = true;
-      main.style.top = -i * sHeight + 'px';
-      setTimeout(function () {
-        sScroll = false
-      }, 700);
+          if(sIndex === sLength - 1) {
+            return;
+          }
+          console.log('向上滑动', sIndex, sLength);
+          scrollTo(sIndex + 1);
+        } else if(Math.abs(distanceX) < Math.abs(distanceY) && distanceY > 0) {
+          console.log('往下滑动', sIndex);
+          if(sIndex === 0) {
+            return;
+          }
+          scrollTo(sIndex -1);
+        } else {
+          // 没有滑动
+        }
+      }
     }
 
   });
